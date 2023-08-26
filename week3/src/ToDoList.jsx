db93f288a9d467bb58a89c2ee6845917373356fd
Kim Jiwon <stayby16@kakao.com>
2023-08-26 17:06:36 +0900

    Read.me 추가

--- week3/src/ToDoList.jsx
+++ week3/src/ToDoList.jsx
@@ -83,20 +83,3 @@ function ToDoList() {
 }
 
 export default ToDoList;
-{
-  /* 질문사항. */
-}
-{
-  /* 왜? 이벤트 리스너 안에서 매개변수를 주려면 arrow function으로 감싸줘야하는지 → 그냥 함수만 넣으면 바로 실행인데,
-  추가로 익명의 함수를 씌워서 누른 다음 실행되도록? */
-}
-{
-  /* removeBtn={() => removeBtn(item.id)}
-     onToggle={() => onToggle(item.id)} 부모요소에서 이렇게 넘겨줘야 하는 이유 */
-}
-{
-  /* onToggle 쪽 코드 리뷰.. */
-}
-{
-  /* 컴포넌트를 분리하는 부분 다시 확인 필요. */
-}
